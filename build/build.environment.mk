4c449cb526f547bfba3e2e85469d6959cdc4cfd7
Ruben Vermeersch <ruben@savanne.be>
2010-02-28 15:09:54 +0100

    First tiny building skeleton.

---
+++ build/build.environment.mk
@@ -0,0 +1,27 @@
+# Initializers
+MONO_BASE_PATH = 
+MONO_ADDINS_PATH =
+
+# Install Paths
+DEFAULT_INSTALL_DIR = $(pkglibdir)
+
+# External libraries to link against, generated from configure
+LINK_SYSTEM = -r:System
+LINK_MONO_POSIX = -r:Mono.Posix
+LINK_HYENA = $(BANSHEE_HYENA_LIBS)
+
+DIR_BIN = $(top_builddir)/bin
+
+# Core
+REF_TRIPOD_CORE = $(LINK_MONO_POSIX) $(LINK_HYENA)
+LINK_TRIPOD_CORE = -r:$(DIR_BIN)/Tripod.Core.dll
+LINK_TRIPOD_CORE_DEPS = $(REF_TRIPOD_CORE) $(LINK_TRIPOD_CORE)
+
+# Cute hack to replace a space with something
+colon:= :
+empty:=
+space:= $(empty) $(empty)
+
+# Build path to allow running uninstalled
+RUN_PATH = $(subst $(space),$(colon), $(MONO_BASE_PATH))
+
